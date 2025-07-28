02f40eb390f6dc0a0db0882f00e54df1c7c4c373
Ben Milligan <122348541+BMMilligan1@users.noreply.github.com>
2025-07-28 11:32:31 +1000

    Update footer with custom text and current date
    Replaces the default footer with a custom message including the Centre for Sustainable Development Reform and the current date formatted as 'MMM DD, YYYY'.

--- observablehq.config.js
+++ observablehq.config.js
@@ -30,7 +30,7 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: `Centre for Sustainable Development Reform on ${new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}.`,
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
